8bbd5b6968eebb73696debabac7df678ee16f06e
Arturo1289 <shadowe543@gmail.com>
2022-12-22 23:44:18 -0600

    to_the_limit

--- SU_AnalistaDeDatos_EDC-main/proyecto_final/juego.py
+++ SU_AnalistaDeDatos_EDC-main/proyecto_final/juego.py
@@ -1,127 +1,28 @@
-import os
-import random
-
-palabraAdivinada = []
-letrasEscritas = []
-intentos = 6
-palabraSecretaAjustes = "1"
-nombreArchivoGrupos = "grupos.txt"
-
-
-def prepararPalabra(original):
-    global palabraAdivinada
-    original = original.lower()
-    palabraAdivinada = []
-    for letra in original:
-        palabraAdivinada.append({
-            "letra": letra,
-            "adivinada": False,
-        })
+import dibujo as dibujarIntentos
+import dibujo as imprimirAhorcado
+import desarrollo as descubrirLetra
+import desarrollo as haGanado
+import desarrollo as imprimirPalabra
+import desarrollo as prepararPalabra
+import desarrollo as imprimirPalabraOriginal
 
 
-def imprimirPalabra():
-    for letraCompuesta in palabraAdivinada:
-        if letraCompuesta["adivinada"]:
-            print(letraCompuesta["letra"], end="")
-        else:
-            print("-", end="")
-    print("")
-
-
-def imprimirPalabraOriginal():
-    for letraCompuesta in palabraAdivinada:
-        print(letraCompuesta["letra"], end="")
-
-
-def descubrirLetra(letraDeUsuario):
-    global palabraAdivinada
+def jugar():
     global letrasEscritas
     global intentos
-    letraDeUsuario = letraDeUsuario.lower()
-    if letraDeUsuario in letrasEscritas:
-        return
-    else:
-        letrasEscritas.append(letraDeUsuario)
-    if not letraEstaEnPalabra(letraDeUsuario):
-        intentos -= 1
-    else:
-        for letraCompuesta in palabraAdivinada:
-            if letraCompuesta["letra"] == letraDeUsuario:
-                letraCompuesta["adivinada"] = True
-
-
-def letraEstaEnPalabra(letra):
-    global palabraAdivinada
-    for letraCompuesta in palabraAdivinada:
-        if letraCompuesta["letra"] == letra:
-            return True
-    return False
-
-
-def imprimirAhorcado():
-    if intentos == 1:
-        print("""
-                       ___
-                      |   |
-                     _O/  |
-                      |   |
-                     / \  |
-                    ______|
-        """)
-    elif intentos == 2:
-        print("""
-                       ___
-                      |   |
-                     _O/  |
-                      |   |
-                       \  |
-                    ______|
-        """)
-    elif intentos == 3:
-        print("""
-                       ___
-                      |   |
-                     _O/  |
-                      |   |
-                          |
-                    ______|
-        """)
-    elif intentos == 4:
-        print("""
-                       ___
-                      |   |
-                     _O/  |
-                          |
-                          |
-                    ______|
-        """)
-    elif intentos == 5:
-        print("""
-                       ___
-                      |   |
-                      O/  |
-                          |
-                          |
-                    ______|
-        """)
-    elif intentos == 6:
-        print("""
-                       ___
-                      |   |
-                      O   |
-                          |
-                          |
-                    ______|
-        """)
-
-
-def dibujarIntentos():
-    print("Intentos restantes: " + str(intentos))
-
-
-def haGanado():
-    global palabraAdivinada
-    for letra in palabraAdivinada:
-        if not letra["adivinada"]:
-            return False
-    return True
+    intentos = 6
+    letrasEscritas = []
+    palabra = obtenerPalabra()
+    prepararPalabra(palabra)
+    while True:
+        imprimirAhorcado()
+        dibujarIntentos()
+        imprimirPalabra()
+        descubrirLetra(input("Ingresa la letra: "))
+        if intentos <= 0:
+            print("Perdiste. La palabra era: ")
+            imprimirPalabraOriginal()
+            return
+        if haGanado():
+            print("Ganaste")
+            return
--- SU_AnalistaDeDatos_EDC-main/proyecto_final/main.py
+++ SU_AnalistaDeDatos_EDC-main/proyecto_final/main.py
@@ -1,31 +1,21 @@
 import instrucciones
 import juego as jugar
-import ajustes
-def prepararArchivo():
-    None
 
 def menu_principal():
     menu = """
 1. Jugar
 2. Instrucciones
-3. Ajustes
-4. Salir
+3. Salir
 Seleccione: """
     eleccion = int(input(menu))
-    if eleccion <= 0 or eleccion >= 4:
+    if eleccion <= 0 or eleccion >= 3:
         exit()
     if eleccion == 1:
         jugar()
     elif eleccion == 2:
         instrucciones()
-    elif eleccion == 3:
-        ajustes()
-
 
 def main():
-    prepararArchivo()
-    while True:
-        menu_principal()
-
+    menu_principal()
 
 main()
