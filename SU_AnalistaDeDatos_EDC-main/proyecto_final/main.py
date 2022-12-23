649ebac9aa8b63edb3b6f341c64eb1954d04ff73
Arturo1289 <shadowe543@gmail.com>
2022-12-23 01:37:13 -0600

    esperanza

--- SU_AnalistaDeDatos_EDC-main/proyecto_final/main.py
+++ SU_AnalistaDeDatos_EDC-main/proyecto_final/main.py
@@ -1,22 +1,30 @@
-import instrucciones
+import instrucciones as instrucciones
 import juego as jugar
+import ajustes as ajustes
+import ajustes as prepararArchivo
 
 def menu_principal():
     menu = """
 1. Jugar
 2. Instrucciones
-3. Salir
+3. Ajustes
+4. Salir
 Seleccione: """
     eleccion = int(input(menu))
-    if eleccion <= 0 or eleccion >= 3:
+    if eleccion <= 0 or eleccion >= 4:
         exit()
     if eleccion == 1:
         jugar()
     elif eleccion == 2:
         instrucciones()
+    elif eleccion == 3:
+        ajustes()
+
 
 def main():
-    True
-    menu_principal()
+    prepararArchivo()
+    while True:
+        menu_principal()
+
 
 main()
